1459cf6d03e794da9fb19dc15968e94c9a52824f
unknown <dlkl46.bl@gmail.com>
2025-02-13 16:10:38 -0700

    Chef

---
+++ ror2_loadout_randomizer/survivors/chef.js
@@ -0,0 +1,57 @@
+export const chef = {
+    name: "Chef",
+    color: "#bd3725",
+    img_dir: "ror2_loadout_randomizer/images/survivors/chef/",
+    portrait: "Chef.webp",
+    skills: [
+        {
+            type: "Passive",
+            choices: [
+                {
+                    name: "Chef's Kiss",
+                    icon: "Chef's_Kiss.webp"
+                }
+            ]
+        },
+        {
+            type: "Primary",
+            choices: [
+                {
+                    name: "Dice",
+                    icon: "Dice.webp"
+                }
+            ]
+        },
+        {
+            type: "Secondary",
+            choices: [
+                {
+                    name: "Sear",
+                    icon: "Sear.webp"
+                }
+            ]
+        },
+        {
+            type: "Utility",
+            choices: [
+                {
+                    name: "Roll",
+                    icon: "Roll.webp"
+                }
+            ]
+        },
+        {
+            type: "Special",
+            choices: [
+                {
+                    name: "Glaze",
+                    icon: "Glaze.webp"
+                },
+                {
+                    name: "Yes, Chef!",
+                    icon: "Yes,_Chef!.webp"
+                },
+            ]
+        },
+    ]
+}
