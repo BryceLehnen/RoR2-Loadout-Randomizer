ceb043ccbabb8ff7126f6e854450edfc2f747f97
unknown <dlkl46.bl@gmail.com>
2025-02-13 15:56:09 -0700

    Seeker

---
+++ ror2_loadout_randomizer/survivors/seeker.js
@@ -0,0 +1,57 @@
+export const seeker = {
+    name: "Seeker",
+    color: "#29937f",
+    img_dir: "ror2_loadout_randomizer/images/survivors/seeker/",
+    portrait: "Seeker.webp",
+    skills: [
+        {
+            type: "Passive",
+            choices: [
+                {
+                    name: "Inner Strength",
+                    icon: "Inner_Strength.webp"
+                }
+            ]
+        },
+        {
+            type: "Primary",
+            choices: [
+                {
+                    name: "Spirit Punch",
+                    icon: "Spirit_Punch.webp"
+                }
+            ]
+        },
+        {
+            type: "Secondary",
+            choices: [
+                {
+                    name: "Unseen Hand",
+                    icon: "Unseen_Hand.webp"
+                },
+                {
+                    name: "Soul Spiral",
+                    icon: "Soul_Spiral.webp"
+                }
+            ]
+        },
+        {
+            type: "Utility",
+            choices: [
+                {
+                    name: "Sojourn",
+                    icon: "Sojourn.webp"
+                }
+            ]
+        },
+        {
+            type: "Special",
+            choices: [
+                {
+                    name: "Meditate",
+                    icon: "Meditate.webp"
+                }
+            ]
+        }
+    ]
+}
