95de1fd077e243cce2fb73b015422850e5b32b65
unknown <dlkl46.bl@gmail.com>
2025-02-13 16:04:44 -0700

    False Son

---
+++ ror2_loadout_randomizer/survivors/false_son.js
@@ -0,0 +1,66 @@
+export const false_son = {
+    name: "False Son",
+    color: "#40b7fa",
+    img_dir: "ror2_loadout_randomizer/images/survivors/false_son/",
+    portrait: "False_Son.webp",
+    skills: [
+        {
+            type: "Passive",
+            choices: [
+                {
+                    name: "Lunar Temapering",
+                    icon: "Lunar_Tampering.webp"
+                }
+            ]
+        },
+        {
+            type: "Primary",
+            choices: [
+                {
+                    name: "Club of the Forsaken",
+                    icon: "Club_of_the_Forsaken.webp"
+                }
+            ]
+        },
+        {
+            type: "Misc",
+            choices: [
+                {
+                    name: "Club Slam",
+                    icon: "Club_Slam.webp"
+                }
+            ]
+        },
+        {
+            type: "Secondary",
+            choices: [
+                {
+                    name: "Lunar Spikes",
+                    icon: "Lunar_Spikes.webp"
+                }
+            ]
+        },
+        {
+            type: "Utility",
+            choices: [
+                {
+                    name: "Step of the Brothers",
+                    icon: "Step_of_the_Brothers.webp"
+                }
+            ]
+        },
+        {
+            type: "Special",
+            choices: [
+                {
+                    name: "Laser of the Father",
+                    icon: "Laser_of_the_Father.webp"
+                },
+                {
+                    name: "Laser Burst",
+                    icon: "Laser_Burst.webp"
+                }
+            ]
+        }
+    ]
+}
