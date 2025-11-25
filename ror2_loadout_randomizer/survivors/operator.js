a2e4940ffd75ab2691ba16ca55af7d4018823b84
unknown <dlkl46.bl@gmail.com>
2025-11-24 17:48:27 -0700

    operator survivor

---
+++ ror2_loadout_randomizer/survivors/operator.js
@@ -0,0 +1,99 @@
+export const Operator = {
+    name: "Operator",
+    color: "#0a7165",
+    img_dir: "ror2_loadout_randomizer/images/survivors/operator/",
+    portrait: "Operator.webp",
+    skills: [
+        {
+            type: "Passive",
+            choices: [
+                {
+                    name: "Drone Tech",
+                    icon: "Drone_Tech.webp"
+                }
+            ]
+        },
+        {
+            type: "Primary",
+            choices: [
+                {
+                    name: "H3-11 OCR Custom",
+                    icon: "H3-11_OCR_Custom.webp"
+                }
+            ]
+        },
+        {
+            type: "Secondary",
+            choices: [
+                {
+                    name: "ADMIN-OVERRIDE",
+                    icon: "ADMIN-OVERRIDE.webp"
+                },
+                {
+                    name: "CMD-SWARM",
+                    icon: "CMD-SWARM.webp"
+                }
+            ]
+        },
+        {
+            type: "Utility",
+            choices: [
+                {
+                    name: "Ascent Protocol",
+                    icon: "Ascent_Protocol.webp"
+                },
+                {
+                    name: "FIREWALL",
+                    icon: "FIREWALL.webp"
+                }
+            ]
+        },
+        {
+            type: "Special",
+            choices: [
+                {
+                    name: "Ejection Core",
+                    icon: "Ejection_Core.webp"
+                },
+                {
+                    name: "Amp Core",
+                    icon: "Amp_Core.webp"
+                }
+            ]
+        },
+        {
+            type: "Drone 1",
+            choices: [
+                {
+                    name: "Gunner Drone Mk2",
+                    icon: "Gunner_Drone_Mk_2.webp"
+                },
+                {
+                    name: "Healing Drone Mk2",
+                    icon: "Healing_Drone_Mk_2.webp"
+                },
+                {
+                    name: "Transport Drone Mk2",
+                    icon: "Transport_Drone_Mk_2.webp"
+                }
+            ]
+        },
+        {
+            type: "Drone 2",
+            choices: [
+                {
+                    name: "Gunner Drone Mk2",
+                    icon: "Gunner_Drone_Mk_2.webp"
+                },
+                {
+                    name: "Healing Drone Mk2",
+                    icon: "Healing_Drone_Mk_2.webp"
+                },
+                {
+                    name: "Transport Drone Mk2",
+                    icon: "Transport_Drone_Mk_2.webp"
+                }
+            ]
+        }
+    ]
+}
