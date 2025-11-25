e716aba3230f605576edaee6b2980e9dbe52099a
unknown <dlkl46.bl@gmail.com>
2025-11-24 17:56:33 -0700

    drifter survivor

---
+++ ror2_loadout_randomizer/survivors/drifter.js
@@ -0,0 +1,65 @@
+export const Drifter = {
+    name: "Drifter",
+    color: "#e79236",
+    img_dir: "ror2_loadout_randomizer/images/survivors/drifter/",
+    portrait: "Drifter.webp",
+    skills: [
+        {
+            type: "Passive",
+            choices: [
+                {
+                    name: "Trash to Treasure",
+                    icon: "Trash_to_Treasure.webp"
+                }
+            ]
+        },
+        {
+            type: "Primary",
+            choices: [
+                {
+                    name: "Blunt Force",
+                    icon: "Blunt_Force.webp"
+                }
+            ]
+        },
+        {
+            type: "Secondary",
+            choices: [
+                {
+                    name: "Cleanup",
+                    icon: "Cleanup.webp"
+                },
+                {
+                    name: "Junk Cube",
+                    icon: "Junk_Cube.webp"
+                }
+            ]
+        },
+        {
+            type: "Utility",
+            choices: [
+                {
+                    name: "Repossess",
+                    icon: "Repossess.webp"
+                },
+                {
+                    name: "Tornado Slam",
+                    icon: "Tornado_Slam.webp"
+                }
+            ]
+        },
+        {
+            type: "Special",
+            choices: [
+                {
+                    name: "Salvage",
+                    icon: "Salvage.webp"
+                },
+                {
+                    name: "Tinker",
+                    icon: "Tinker.webp"
+                }
+            ]
+        }
+    ]
+}
